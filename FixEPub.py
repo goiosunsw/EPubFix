b85285abef052dbad27eb11321d8259c8f9b0ab2
Andre Almeida <a.almeida@unsw.edu.au>
2016-01-17 22:27:44 +1100

    Project created

---
+++ FixEPub.py
@@ -0,0 +1,75 @@
+import zipfile
+import os
+import shutil
+import tempfile
+import xml.etree.ElementTree as ET
+
+class epub_file:
+    def __init__(self,epub_file):
+        epub_basename = os.path.splitext(epub_file)[0]
+        epub_dirname = os.path.basename(epub_basename)
+        destdir = os.path.join(tempfile.gettempdir(),'FixEPub',epub_dirname)
+        try:
+            os.makedirs(destdir)
+        except OSError:
+            if os.path.isdir(destdir):
+                # if directory exists, clean it
+                shutil.rmtree(destdir)
+            else:
+                raise
+        
+        self.epub_file = epub_file
+        self.new_epub = epub_basename + '_fixed.epub'
+        self.epub_tree = destdir
+        self.unzip_epub(destdir)
+        
+        ## Manifest data
+        self.content_file='META-INF/container.xml'
+        self.content_namespace = '{urn:oasis:names:tc:opendocument:xmlns:container}'
+        self.opf_namespace = '{http://www.idpf.org/2007/opf}'
+        
+        self.opf_file = self.get_content_file()
+    
+    def unzip_epub(self,dest_dir):
+        with zipfile.ZipFile(self.epub_file) as zf:
+            zf.extractall(dest_dir)
+            
+    def zip_epub(self):
+        with zipfile.ZipFile(self.new_epub, 'w', zipfile.ZIP_DEFLATED) as zf:
+            for dirpath, dirnames, filenames in os.walk(self.epub_tree):
+                relpath = os.path.relpath(dirpath,self.epub_tree)
+                for file in filenames:
+                    src = os.path.join(dirpath,file)
+                    dest = os.path.join(relpath,file)
+                    zf.write(src,dest)
+        zf.close()
+        
+    def get_content_file(self):
+        contxml=ET.parse(os.path.join(self.epub_tree,self.content_file))        
+        contfile=contxml.find(".//*"+self.content_namespace+"rootfile").attrib['full-path']
+        return contfile
+        
+    def get_ordered_html_list(self):
+        opf_file = self.get_content_file()
+        opfxml = ET.parse(os.path.join(self.epub_tree,opf_file))
+        toclist = opfxml.findall(".//*{http://www.idpf.org/2007/opf}itemref")
+        htmlfiles = []
+        for tocitem in toclist:
+            htid = tocitem.attrib['idref']
+            html_item = opfxml.find(".//*[@id='"+htid+"']")
+            html_file = html_item.attrib['href']
+            htmlfiles.append(html_file)
+            
+        return htmlfiles
+            
+        
+        
+        
+        
+    def finish(self):
+        '''Rezip the epub file and cleanup temp dir'''
+        self.zip_epub()
+        shutil.rmtree(self.epub_tree)
+
+
+        
---
+++ README.md
@@ -0,0 +1,14 @@
+# EPubFix -- 
+# Tool to fix EPub files converted from PDF
+
+## Aim
+
+Epub files converted from PDF often have common problems such as
+* Header / footer text amid the running text
+** Page numbers
+** Running headers
+* Pagraph breaks when clraly there isn't a new paragraph as newline begins with a lowercase letter
+
+## Fix
+
+EPubFix attempts to fix these problems
